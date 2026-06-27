47d7ce98f6235040214dde19fa17d6133a276dc0
Solid Snake <sohamkinage691@gmail.com>
2026-06-27 16:50:27 +0530

    Create intersection_of_two_arrays.cpp
    Problem: Intersection of Two Arrays (LeetCode 349)
    
    Approach:
    1. Sort both arrays.
    2. Use two pointers (i and j).
    3. If elements are equal, add them to the answer (only if it is not already present).
    4. If nums1[i] < nums2[j], move i forward.
    5. Otherwise, move j forward.
    
    Time Complexity: O(n log n + m log m)
    Space Complexity: O(1) excluding the answer vector.

---
+++ Array/intersection_of_two_arrays.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+
+    int i = 0, j = 0;
+    vector<int> ans;
+
+    while (i < nums1.size() && j < nums2.size()) {
+        if (nums1[i] == nums2[j]) {
+            if (ans.empty() || ans.back() != nums1[i]) {
+                ans.push_back(nums1[i]);
+            }
+            i++;
+            j++;
+        }
+        else if (nums1[i] < nums2[j]) {
+            i++;
+        }
+        else {
+            j++;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+
+    vector<int> ans = intersection(nums1, nums2);
+
+    cout << "Intersection: ";
+    for (int x : ans) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
